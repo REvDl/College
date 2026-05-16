68820b48dfd298c147694198d26dadca6497bb01
REvDi <peresadok33@gmail.com>
2026-05-16 18:45:19 +0300

    Leetcode 154 findMin (C++)

---
+++ Algorithmic_practice/hard/findMin.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int left = 0, right = static_cast<int>(nums.size()) - 1;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            }
+            else if (nums[mid] == nums[right]) {
+                right -= 1;
+            }
+            else right = mid;
+            
+        }
+        return nums[left];
+    }
+};
+
+
+int main() {
+    cout << "Suck" << endl;
+    return 0;
+}
