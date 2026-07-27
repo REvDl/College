27e401bf38e5975d0288b2e4c51eb9e842c5e388
REvDi <peresadok33@gmail.com>
2026-07-27 16:49:48 +0300

    Leetcode 1464 maxProduct

---
+++ Algorithmic_practice/easy/maxProduct_1464.cpp
@@ -0,0 +1,13 @@
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+  int maxProduct(vector<int> &nums) {
+    int n = nums.size();
+    sort(nums.begin(), nums.end());
+    return (nums[n - 2] - 1) * (nums[n - 1] - 1);
+  }
+};
